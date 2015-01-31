08823e4f2cbad32e8bd96e88c92ed59aa9fcb4d9
Pascal DENIS <pascal.denis.75@gmail.com>
2015-01-31 15:55:51 +0100

    Service cacheable listener

--- src/Itkg/Consumer/Listener/ServiceCacheableListener.php
+++ src/Itkg/Consumer/Listener/ServiceCacheableListener.php
@@ -2,7 +2,94 @@
 
 namespace Itkg\Consumer\Listener;
 
-class ServiceCacheableListener
+use Itkg\Consumer\Event\ServiceEvent;
+use Itkg\Consumer\Event\ServiceEvents;
+use Itkg\Consumer\Service\ServiceCacheable;
+use Itkg\Core\Cache\AdapterInterface;
+use Itkg\Core\Cache\Event\CacheEvent;
+use Symfony\Component\EventDispatcher\EventDispatcher;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+class ServiceCacheableListener implements EventSubscriberInterface
 {
-    
+    /**
+     * @var AdapterInterface
+     */
+    private $cacheAdapter;
+
+    /**
+     * @var EventDispatcher
+     */
+    private $eventDispatcher;
+
+    public function __construct(AdapterInterface $cacheAdapter, EventDispatcher $eventDispatcher)
+    {
+        $this->cacheAdapter = $cacheAdapter;
+    }
+
+    /**
+     * @param ServiceEvent $event
+     */
+    public function onServiceRequest(ServiceEvent $event)
+    {
+        $service = $event->getService();
+
+        if (!$service instanceof ServiceCacheable) {
+            return;
+        }
+
+        // Check cache
+        if (false !== $data = $this->cacheAdapter->get($service)) {
+
+            // Set data from cache to entity object
+            $service->setDataFromCache($data);
+            $service->setIsLoaded(true);
+            $this->eventDispatcher->dispatch('cache.load', new CacheEvent($service));
+        }
+    }
+
+    /**
+     * @param ServiceEvent $event
+     */
+    public function onServiceResponse(ServiceEvent $event)
+    {
+        $service = $event->getService();
+
+        if (!$service instanceof ServiceCacheable) {
+            return;
+        }
+
+        if (!$service->isLoaded()) {
+            $this->cacheAdapter->set($service);
+            $this->eventDispatcher->dispatch('cache.set', new CacheEvent($service));
+        }
+    }
+
+    /**
+     * Returns an array of event names this subscriber wants to listen to.
+     *
+     * The array keys are event names and the value can be:
+     *
+     *  * The method name to call (priority defaults to 0)
+     *  * An array composed of the method name to call and the priority
+     *  * An array of arrays composed of the method names to call and respective
+     *    priorities, or 0 if unset
+     *
+     * For instance:
+     *
+     *  * array('eventName' => 'methodName')
+     *  * array('eventName' => array('methodName', $priority))
+     *  * array('eventName' => array(array('methodName1', $priority), array('methodName2'))
+     *
+     * @return array The event names to listen to
+     *
+     * @api
+     */
+    public static function getSubscribedEvents()
+    {
+        return array(
+            ServiceEvents::REQUEST  => 'onServiceRequest',
+            ServiceEvents::RESPONSE => 'onServiceResponse'
+        );
+    }
 }
