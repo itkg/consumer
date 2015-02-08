c0703f1282c2d748bc05d391d4ae188f99b62915
Pascal DENIS <pascal.denis.75@gmail.com>
2015-02-08 15:59:31 +0100

    rest client tests

---
+++ tests/Itkg/Consumer/Client/RestClientTest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Itkg\Consumer\Client;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class RestClientTest extends \PHPUnit_Framework_TestCase
+{
+    public function testSendRequest()
+    {
+        $client = new RestClient();
+        $response = new Response();
+        $client->sendRequest(
+            Request::create('http://www.google.fr'),
+            $response
+        );
+
+        $this->assertTrue($response->isSuccessful());
+        $this->assertNotNull($response->getContent());
+        $this->assertNotEmpty($response->headers->all());
+    }
+
+    public function testMethods()
+    {
+        $client = $this->getMock('Itkg\Consumer\Client\RestClient', array('sendRequest'));
+
+        $client->expects($this->at(0))->method('post');
+        $client->expects($this->at(1))->method('put');
+        $client->expects($this->at(2))->method('delete');
+
+        $response = new Response();
+        $request = Request::create('http://www.google.fr', 'POST');
+
+        $client->sendRequest(
+            $request,
+            $response
+        );
+
+        $request = Request::create('http://www.google.fr', 'PUT');
+
+        $client->sendRequest(
+            $request,
+            $response
+        );
+
+        $request = Request::create('http://www.google.fr', 'DELETE');
+
+        $client->sendRequest(
+            $request,
+            $response
+        );
+    }
+}
