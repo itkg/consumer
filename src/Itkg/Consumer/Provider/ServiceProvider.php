b906589d43cc01107c3d0ec1279558673e6cf932
Pascal DENIS <pascal.denis.75@gmail.com>
2015-02-28 11:41:08 +0100

    remove double deserializer listener definition in provider Fix service doc

--- src/Itkg/Consumer/Provider/ServiceProvider.php
+++ src/Itkg/Consumer/Provider/ServiceProvider.php
@@ -8,6 +8,13 @@ use Itkg\Consumer\Listener\LoggerListener;
 use Itkg\Core\Provider\ServiceProviderInterface;
 use JMS\Serializer\SerializerBuilder;
 
+/**
+ * Class ServiceProvider
+ *
+ * A provider for Core container
+ *
+ * @package Itkg\Consumer\Provider
+ */
 class ServiceProvider implements ServiceProviderInterface
 {
     /**
@@ -36,21 +43,11 @@ class ServiceProvider implements ServiceProviderInterface
             }
         );
 
-        $container['deserializer_listener'] = $mainContainer->share(
-            function () {
-                return new DeserializerListener(
-                    SerializerBuilder::create()->build()
-                );
-            }
-        );
-
         $container['cache_listener'] = $mainContainer->share(
             function () use ($mainContainer) {
                 return new CacheListener($mainContainer['core']['dispatcher']);
             }
         );
-
-
 
         $mainContainer['core']['dispatcher']->addSubscriber($container['cache_listener']);
         $mainContainer['core']['dispatcher']->addSubscriber($container['deserializer_listener']);
